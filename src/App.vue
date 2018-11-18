ace5343f1502baa97973daaaa8875fcbd8bc85ff
shirin <eghrarish@gmail.com>
2018-11-18 12:27:16 +0330

    Worked on main layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <simple-swiper>
+      
+    </simple-swiper>
   </div>
 </template>
 
 <script>
-
+import SimpleSwiper from './components/SimpleSwiper.vue'
 export default {
   name: 'app',
   components: {
-  }
+    SimpleSwiper
+    }
 }
 </script>
--- src/components/SimpleSwiper.vue
+++ src/components/SimpleSwiper.vue
@@ -1,14 +1,42 @@
 <template>
-  <div class="simpleSwiper">
+  <div class="simple-swiper" :style="slideStyle">
+    <img src="./../assets/left-arrow.svg">
+    <div>
+      <slot></slot>
+    </div>
+    <img src="./../assets/right-arrow.svg">
   </div>
 </template>
 
 <script>
 export default {
   name: 'SimpleSwiper',
+  computed: {
+    slideStyle () {
+      return {
+        width: `${this.width}px`,
+        height: `${this.height}px`
+      }
+    }
+  },
   props: {
+    count: {
+      type: Number,
+      default: 0
+    },
+    width: {
+      type: Number,
+      default: 80
+    },
+    height: {
+      type: Number,
+      default: 80
+    }
   }
 }
 </script>
 <style lang="sass">
+.simple-swiper
+  display: grid
+  grid-template-column: auto 1fr auto
 </style>
